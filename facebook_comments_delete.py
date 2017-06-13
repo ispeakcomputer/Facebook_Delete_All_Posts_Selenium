7c1f456db8b4bfcea0dcdea2b215b4db01562dd7
ryan <mail@interwebz.space>
2017-06-13 16:13:07 -0600

    OK, the script is pretty hacky, but we are just getting started. I am able to login and navigate all the way through to the last delete confrimation button. I am still playing with XPath in order to master getting around the DOM. I removed my username and password to save this version . Will need to be added back in to continue. These varibles will be python raw input data once the script is done

--- facebook_comments_delete.py
+++ facebook_comments_delete.py
@@ -12,26 +12,46 @@ driver = None
 
 def load_driver():
 	global driver
-	chromedriver = os.path.abspath("./chromedriver")
-	os.environ["webdriver.chrome.driver"] = chromedriver
-	driver = webdriver.Chrome(chromedriver)
+	driver = webdriver.Chrome()
 
 
 def log_in(username, password):
+    global driver
+    driver.get("https://m.facebook.com")
+    username_elem = driver.find_element_by_name("email")
+    username_elem.clear()
+    username_elem.send_keys(username)
+    password_elem = driver.find_element_by_name("pass")
+    password_elem.clear()
+    password_elem.send_keys(password)
+    password_elem.send_keys(Keys.RETURN)
+    time.sleep(2) # Adjust according to your Internet speed
+    continue_link = driver.find_element_by_link_text('Not Now')
+    continue_link.click()
+    time.sleep(2)
+    goto_profile_menu = driver.find_elements_by_class_name("_59tf")
+    goto_profile_menu[5].click()
+    time.sleep(2)
+    goto_profile = driver.find_elements_by_class_name("_52x3")
+    goto_profile[0].click()
+    time.sleep(2)
+    delete_post_button_list = driver.find_element_by_class_name('_4s19')
+    delete_post_button_list.click()
+    time.sleep(2)
+    drop_down_delete = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div/div/div[2]/a[26]')
+    drop_down_delete.click()
+    time.sleep(2)
+    final_delete_button = driver.find_element_by_xpath("//*[@title='Delete'")
+
+
+def click_to_profile():
 	global driver
-	driver.get("https://m.facebook.com")
-	username_elem = driver.find_element_by_name("email")
-	username_elem.clear()
-	username_elem.send_keys(username)
-	password_elem = driver.find_element_by_name("pass")
-	password_elem.clear()
-	password_elem.send_keys(password)
-	password_elem.send_keys(Keys.RETURN)
-	time.sleep(3) # Adjust according to your Internet speed
+
 
 if __name__ == "__main__":
-	username = raw_input("Enter your Facebook username: ")
-	password = getpass.getpass("Enter your Facebook password: ")
-	target = raw_input("Enter the target's Facebook username: ")
+	
+	# username = raw_input("Enter your Facebook username: ")
+	# password = getpass.getpass("Enter your Facebook password: ")
+	# target = raw_input("Enter the target's Facebook username: ")
 	load_driver()
 	log_in(username, password)
