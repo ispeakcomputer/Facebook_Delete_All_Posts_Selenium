2fb3c70bba165aa194a8cddb097b143563715ee4
ryan <mail@interwebz.space>
2017-06-13 17:00:44 -0600

    just need to run and test the automation and cleanup. Looks to be pretty much done. It still needs some formatting and maybe a refactor

--- facebook_comments_delete.py
+++ facebook_comments_delete.py
@@ -11,12 +11,13 @@ driver = None
 
 
 def load_driver():
-	global driver
-	driver = webdriver.Chrome()
+    global driver
+    driver = webdriver.Chrome()
 
 
 def log_in(username, password):
     global driver
+    counter = 0
     driver.get("https://m.facebook.com")
     username_elem = driver.find_element_by_name("email")
     username_elem.clear()
@@ -35,23 +36,32 @@ def log_in(username, password):
     goto_profile = driver.find_elements_by_class_name("_52x3")
     goto_profile[0].click()
     time.sleep(2)
-    delete_post_button_list = driver.find_element_by_class_name('_4s19')
-    delete_post_button_list.click()
-    time.sleep(2)
-    drop_down_delete = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div/div/div[2]/a[26]')
-    drop_down_delete.click()
-    time.sleep(2)
-    final_delete_button = driver.find_element_by_xpath("//*[@title='Delete'")
 
+    while True:
+        try:
+            delete_post_button_list = driver.find_element_by_class_name('_4s19')
+            delete_post_button_list.click()
+            time.sleep(2)
+            drop_down_delete = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div/div/div[2]/a[26]')
+            drop_down_delete.click()
+            time.sleep(2)
+            final_delete_button = driver.find_element_by_xpath("//a[@title='Delete']")
+            final_delete_button.click()
+            time.sleep(2)
+            counter =  counter + 1
+            print("{} posts have been deleted".format(counter))
+        except:
+            print("Your posts have all been deleted.")
 
 def click_to_profile():
-	global driver
+    global driver
 
 
 if __name__ == "__main__":
-	
-	# username = raw_input("Enter your Facebook username: ")
-	# password = getpass.getpass("Enter your Facebook password: ")
-	# target = raw_input("Enter the target's Facebook username: ")
-	load_driver()
-	log_in(username, password)
+    username = ""
+    password = ""
+    # username = raw_input("Enter your Facebook username: ")
+    # password = getpass.getpass("Enter your Facebook password: ")
+    # target = raw_input("Enter the target's Facebook username: ")
+    load_driver()
+    log_in(username, password)
